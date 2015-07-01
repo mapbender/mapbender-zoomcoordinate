15afb9055f327dc88ee07d7962a7c2779e86e917
Tomy Keller <tomy.keller@wheregroup.com>
2015-07-01 16:23:27 +0200

    update

--- src/Mapbender/ZoomcoordinateBundle/Element/Zoomcoordinate.php
+++ src/Mapbender/ZoomcoordinateBundle/Element/Zoomcoordinate.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Mapbender\AlkisBundle\Element;
+namespace Mapbender\Zoomcoordinate\Element;
 
 use Mapbender\CoreBundle\Component\Element;
 use Symfony\Component\HttpFoundation\Response;
